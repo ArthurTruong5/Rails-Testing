d5f29cf07d8ad8eeea7307da5df6c8265711a30c
Arthur Truong <arthurtruong.work@gmail.com>
2018-05-11 16:41:52 +1000

    Intergrated skills via model generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_09_023824) do
+ActiveRecord::Schema.define(version: 2018_05_11_055142) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 2018_05_09_023824) do
     t.string "company"
     t.decimal "tax"
     t.string "salesperson"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.integer "percent_utilized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
