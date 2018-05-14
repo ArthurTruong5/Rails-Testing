57a03d251b01081436e90e19f5f9b332a9a9967e
Arthur Truong <arthurtruong.work@gmail.com>
2018-05-14 11:22:48 +1000

    added destroy action for portfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -39,6 +39,17 @@ def update
   end
 end
 
+def destroy
+  # perform the lookup
+  @portfolio_item = Portfolio.find(params[:id])
+  #destroy/delete the record
+  @portfolio_item.destroy
+  #Redirect
+  respond_to do |format|
+    format.html { redirect_to portfolios_url, notice: 'Record was removed' }
+  end
+end
+
 
 
 end
