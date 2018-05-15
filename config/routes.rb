f159c9240f9e3847b9966cef2925b870dae0a949
Arthur Truong <arthurtruong.work@gmail.com>
2018-05-15 11:50:23 +1000

    Implemented toggle status for blogs

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
 
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  resources :blogs
+  resources :blogs do
+    member do
+        get :toggle_status
+      end
+    end
   resources :invoices
 
   root to: 'pages#home' # root path
